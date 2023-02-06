2a4b93506a4de16c642c4b2a6d27cd1dff9829bd
Rafael Almeida <rafaprof312@gmail.com>
2023-02-06 19:19:08 -0300

    Update success.tsx

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -19,7 +19,7 @@ export default function Success() {
         <Image src="https://github.com/rafaalmeida1.png" width={150} height={150} alt="Developer Photo" />
 
         <strong>Espero que estejam satisfeitos com o resultado do teste!</strong>
-        <span>Com muito carinho - <Link href="ttps://github.com/rafaalmeida1">Rafael Almeida</Link></span>
+        <span>Com muito carinho - <Link href="https://github.com/rafaalmeida1">Rafael Almeida</Link></span>
 
         <Link href="/">Voltar ao catálogo</Link>
       </SuccessContainer>
